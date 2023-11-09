47090ae97e135f8f6e987f4eeb373a13b5e4eddd
Juan Carlos Salazar Silva <salazarsilvajc@gmail.com>
2023-11-08 20:01:22 -0800

    Descarga de nuevos HTML y organizando carpetas para scrapear

--- scrap-config-3.py
+++ scrap-config-3.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 
-with open("HTML/ITE-SCRAP-P1.html", "r", encoding="utf-8") as file:
+with open("html/oferta-educativa/licenciaturas-ite/ing-mecatronica.html", "r", encoding="utf-8") as file:
     soup = BeautifulSoup(file, 'html.parser')
 
 elements = soup.find_all(['p', 'h1', 'h2'])
@@ -16,4 +16,3 @@ for element in elements:
     ]
     if not any(spam_frase in text for spam_frase in spam_frases):
         print(text)
-        
